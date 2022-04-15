dd58fab61a19ba42f8d57ac02ab35ff93a35cc6c
schel-d <schel.d.dev@gmail.com>
2022-04-15 20:20:59 +1000

    download and extract data.zip

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 import { main } from "./.out/main.js";
-main();
+await main();
--- ts/fetch-data.ts
+++ ts/fetch-data.ts
@@ -1,7 +1,13 @@
-import fetch from "node-fetch";
+import extract from "extract-zip";
+import { createWriteStream, existsSync, mkdir, rm } from "fs";
+import got from "got";
+import { dirname, resolve } from "path";
+import { pipeline, PipelineSource } from "stream";
+import { promisify } from "util";
 
 const DATA_URL = "https://data.trainarrives.in/latest.json";
 const DATA_VERSION = "v1";
+const DATA_TEMP_LOCATION = "./.out/.data/data.zip";
 type LatestJsonType = {
   versions: {
     [version: string]: {
@@ -12,13 +18,56 @@ type LatestJsonType = {
 }
 
 export async function fetchData() {
-  const response = await fetch(DATA_URL);
-  const data = await response.json() as LatestJsonType;
-  const dataVersion = data.versions[DATA_VERSION];
+  let data: LatestJsonType;
+  try {
+    data = await got.get(DATA_URL).json() as LatestJsonType;
+  }
+  catch {
+    throw `There was not a json file at "${DATA_URL}"`;
+  }
 
+  const dataVersion = data.versions[DATA_VERSION];
   if (dataVersion == null) {
     throw `This data server does not provide "${DATA_VERSION}" data (${DATA_URL})`;
   }
 
-  console.log(dataVersion);
+  await prepareEmptyFolder(dirname(DATA_TEMP_LOCATION));
+
+  // Download the latest data, if that fails download the backup data.
+  // This is to account for the case that following a git commit, the server
+  // has deployed the new version of "latest.json" but not the new zip file yet.
+  try {
+    await downloadZip(dataVersion.latest, DATA_TEMP_LOCATION);
+  }
+  catch {
+    try {
+      await downloadZip(dataVersion.backup, DATA_TEMP_LOCATION);
+    }
+    catch {
+      throw `Neither "${dataVersion.latest}" or backup option "${dataVersion.backup}" could be downloaded`;
+    }
+  }
+
+  await extract(DATA_TEMP_LOCATION, { dir: resolve(dirname(DATA_TEMP_LOCATION)) });
+}
+
+const deleteDir = promisify(rm);
+const createDir = promisify(mkdir);
+async function prepareEmptyFolder(path: string) {
+  const shouldDelete = existsSync(path);
+  if (shouldDelete) {
+    await deleteDir(path, { recursive: true });
+  }
+  await createDir(path, { recursive: true });
+}
+
+const pipelineAsync = promisify(pipeline);
+async function downloadZip(path: string, destination: string) {
+  if ((await got.get(path)).headers["content-type"] == "application/zip") {
+    const stream = got.stream(path);
+    await pipelineAsync(stream, createWriteStream(destination));
+  }
+  else {
+    throw `"${path}" was not a zip file`;
+  }
 }
