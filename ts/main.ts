ddf6e9fc5254eee0c8e32425daf15a623ebb878c
schel-d <schel.d.dev@gmail.com>
2022-04-15 18:04:49 +1000

    enforce semicolons

--- ts/main.ts
+++ ts/main.ts
@@ -2,13 +2,13 @@ import express from "express";
 
 export function main() {
   const app = express();
-  const port = process.env.PORT ?? 3000
+  const port = process.env.PORT ?? 3000;
 
   app.get('/', (_, res) => {
-    res.json({ "status": "online" })
-  })
+    res.json({ "status": "online" });
+  });
 
   app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-  })
+    console.log(`Server listening on port ${port}`);
+  });
 }
